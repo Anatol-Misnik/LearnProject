05c3974264c9f2978ba8756c6c77d100055d97d5
Anatol-Misnik <76948715+Anatol-Misnik@users.noreply.github.com>
2022-04-15 13:41:52 +0200

    Create Task1.start.sh

---
+++ Task1.start.sh
@@ -0,0 +1,14 @@
+echo "--------------- Build Started ------------------"
+cat <<EOF > index.html
+<html>
+<body bgcolor=black>
+<center>
+<h2><font color=yellow>Hello Sergej</font></h2>
+
+<font color+blue>www.godeltech.com</font>
+
+</center>
+</body>
+</html>
+EOF
+echo "--------------- Build Finished -----------------"
